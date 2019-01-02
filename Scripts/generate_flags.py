f17e11afac5998e8d5c6da328c4bed38bd8756f3
mstyves <mathieu.st-yves@hotmail.com>
2019-01-02 16:31:28 -0500

    missing backup file

--- Scripts/generate_flags.py
+++ Scripts/generate_flags.py
@@ -95,6 +95,13 @@ config = '{"count": 1, "results": [{"id": 1, "name": "ShawiSec", "email": "ctf@s
 with open('db/config.json', 'w') as file:
     file.write(config)
 
+pages = '{"count": 1, "results": [{"id": 1, "route": "index", "html": "<div class=\"row\">\r\n    <div class=\"col-md-10 offset-md-1\">\r\n        <img class=\"w-100 mx-auto d-block\" style=\"max-width: 1500px;padding-bottom: 50px;\" src=\"/files/d2a23ebe39c3d6dcfac27208d33b6130/image.jpg\" />\r\n        <h3 class=\"text-center\">\r\n            <p>Atelier CTF de la <a href=\"https://shawisec.ca/journee-cybersecurite-0x02/\">Journ\u00e9e Cybers\u00e9curit\u00e9 0x02</a> pr\u00e9sent\u00e9e par <a href=\"https://shawisec.ca/\" style=\"font-family: Copperplate, Montserrat !important;font-size: 2rem;\">ShawiSec</a>!</p>\r\n        </h3>\r\n        \r\n        <p class=\"text-center\"><a href=\"/register\">Inscrivez-vous</a> pour essayer de r\u00e9soudre les <a href=\"/challenges\">d\u00e9fis</a> propos\u00e9s!</p>\r\n    </div>\r\n</div>", "auth_required": 0, "draft": 0, "title": "None"}], "meta": {}}'
+with open('db/pages.json', 'w') as file:
+    file.write(pages)
+
+teams = '{"count": 1, "results": [{"id": 1, "name": "ShawiSec", "email": "ctf@shawisec.ca", "password": "$bcrypt-sha256$2b,12$hXDyspwCrgsRBt5GCN6o6u$BM9.hw6W/Mt4YQTWZEJOtlRx28eynbS", "website": "https://shawisec.ca", "affiliation": "", "country": "Canada", "bracket": null, "banned": 1, "verified": 0, "admin": 1, "joined": "2018-12-16T23:15:14"}], "meta": {}}'
+with open('db/teams.json', 'w') as file:
+    file.write(teams)
 
 def zipdir(path, ziph):
     # ziph is zipfile handle
