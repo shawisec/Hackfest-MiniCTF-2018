8fa29912ffa618f5c5337f1a6a93126eff45c0cf
mstyves <mathieu.st-yves@hotmail.com>
2019-01-03 17:20:46 -0500

    fix priv escalade challenges

--- challenges/priv-2/launch.sh
+++ challenges/priv-2/launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-socat TCP4-LISTEN:5011,fork,tcpwrap=script EXEC:/hf18/challenges/priv-2/docker.sh,pty,stderr
+socat TCP4-LISTEN:5011,fork,tcpwrap=script EXEC:/root/docker.sh,pty,stderr
--- challenges/priv-3/launch.sh
+++ challenges/priv-3/launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-socat TCP4-LISTEN:5012,fork,tcpwrap=script EXEC:/hf18/challenges/priv-3/docker.sh,pty,stderr
+socat TCP4-LISTEN:5012,fork,tcpwrap=script EXEC:/root/docker.sh,pty,stderr
