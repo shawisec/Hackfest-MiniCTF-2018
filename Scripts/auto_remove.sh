e46ced820b41bb5d0fedb9430198a752f0f72bda
mstyves <mathieu.st-yves@hotmail.com>
2019-01-03 17:44:49 -0500

    autoremove

--- Scripts/auto_remove.sh
+++ Scripts/auto_remove.sh
@@ -1,13 +1,11 @@
-
 config_folder=/hf18/deploy/*
 dirs=($(find $config_folder -type d))
 
 for app in "${dirs[@]}";do
   filename="${app##*/}"
-  docker rm ${filename}
+  id=(docker ps -aqf "name=${filename}")
+  docker stop ${id}
   echo "removed: ${filename}"
 done
 
-docker service ls
-
-docker swarm leave --force
+docker ps
