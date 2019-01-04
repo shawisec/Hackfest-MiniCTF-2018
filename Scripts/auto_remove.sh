44749cace017d90b91684eb5d0811cd462a5d04e
mathieu244 <mathieu.st-yves@hotmail.com>
2019-01-03 19:11:34 -0500

    Update auto_remove.sh

--- Scripts/auto_remove.sh
+++ Scripts/auto_remove.sh
@@ -1,10 +1,12 @@
 config_folder=/hf18/deploy/*
 dirs=($(find $config_folder -type d))
 
+docker stop $(docker ps -aq)
+
 for app in "${dirs[@]}";do
   filename="${app##*/}"
   id=(docker ps -aqf "name=${filename}")
-  docker stop ${id}
+  docker rmi ${id} --force
   echo "removed: ${filename}"
 done
 
